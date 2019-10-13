298a1bc30ef26316d5c9dffdb4950a573cf534e8
csw823197541 <823917541@qq.com>
2019-10-13 17:51:01 +0800

    1

--- assets/module/config.js
+++ assets/module/config.js
@@ -10,6 +10,7 @@ layui.define(function (exports) {
         // three_user_server: 'http://127.0.0.1:3333', // 用户服务
         three_log_server: gateway + '/three-log-server', // 日志服务
         three_develop_server: gateway + '/three-develop-server', // 定时任务服务
+        three_auth_server: gateway + '/three-auth-server', // 定时任务服务
         default_script_server: 'three-develop-server', // 默认加载脚本的服务
         tableName: 'cswWeb',  // 存储表名
         autoRender: false,  // 窗口大小改变后是否自动重新渲染表格，解决layui数据表格非响应式的问题，目前实现的还不是很好，暂时关闭该功能
